4f555ce8582673f5da3e073d4372a52b27d8657f
Michael Lima <michael.cdl@outlook.com>
2022-05-05 00:14:42 +0200

    changed routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import TopBar from './components/TopBar/TopBar';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/Home/Home';
 import MemoryGame from './components/Games/IconMemory/memoryGame';
+import MemoryStart from './components/Games/IconMemory/MemoryStart';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router class=' bg-slate-900 ...'>
       <div className="App">
         <TopBar/>
         <Routes >
           <Route path='/' element={<Home/>}/>
-          <Route path='/game-memory' element={<MemoryGame/>}/>
+          <Route path='/game-memory' element={<MemoryStart/>}/>
         </Routes>
       </div>
     </Router>
