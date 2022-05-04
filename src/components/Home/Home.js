c036b407dc835846d89300a447dd48ce0690cc77
Michael Lima <michael.cdl@outlook.com>
2022-05-05 00:17:19 +0200

    new Routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,20 @@ import React, {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import GameCapsule from '../GameCapsule/GameCapsule'
 import list from '../gameList'
+import Hexagon from 'react-hexagon'
 
 
 const  Home= (props) => {
-    
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [screenHeight, setScreenHeight] = useState(window.innerHeight)
+
+
+    useEffect(() => {
+        
+    }, []);
+
     return( 
-        <div class=' bg-slate-900 justify-items-center flex items-center justify-center flex-col'>        
+        <div class=' bg-slate-900 justify-items-center flex items-center justify-center flex-col h-full '>        
             <a href='https://github.com/michlima/jsGames' class='text-2xl bg-purple-600 transition-all hover:scale-125 mt-10 mb-10 h-20 w-1/6  flex items-center justify-center rounded-lg flex-col p-20 ...'>
                 <div class='flex-row'><span class=' font-semibold text-teal-400'>Mi</span>Games </div>
                 <br/><span class='text-xs text-slate-900'>source code</span>    
@@ -25,7 +33,8 @@ const  Home= (props) => {
                 />
                 )
             })}
-            
+            <p class='text-slate-400 text-xs mb-5'> © Copyright Stuff 
+             source code : <a href='https://github.com/michlima/jsGames'>https://github.com/michlima/jsGames</a></p>
         </div>         
     )
 }
