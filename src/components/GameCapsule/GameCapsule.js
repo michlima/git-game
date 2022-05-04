47225a214545d7863dd076e47c868babed84c4f8
Michael Lima <michael.cdl@outlook.com>
2022-05-05 00:16:53 +0200

    New Styling

--- src/components/GameCapsule/GameCapsule.js
+++ src/components/GameCapsule/GameCapsule.js
@@ -5,7 +5,7 @@ const GameCapsule = (props) => {
     
     return (
         <div class=' bg-slate-900 justify-items-center flex items-center justify-center flex-col'>        
-            <Link to={props.link} class=' hover:ml-10 transition-all bg-cyan-600 hover:rounded-full text-slate-50 mt-5 mb-5 w-50  flex items-center justify-center rounded-br-lg p-10 duration-1000 ...'> 
+            <Link to={props.link} class=' hover:ml-10 transition-all bg-cyan-600 rounded-full text-slate-50 mt-5 mb-10 w-50  flex items-center justify-center p-10 duration-1000 ...'> 
                 <p class=''>{props.name }</p>
             </Link>
         </div> 
